24214da00afd9f4c92c6ca244bc1bfd8c5e0d0bf
adib_syarif <asyarif644@gmail.com>
2024-09-19 17:08:16 +0700

    feat (template): added template initial

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,97 @@
-@extends('layouts.app')
+<x-base-layout :scrollspy="false">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <x-slot:pageTitle>
+        {{-- {{$title}}  --}}
+    </x-slot>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <x-slot:headerFiles>
+        <!--  BEGIN CUSTOM STYLE FILE  -->
+        @vite(['resources/scss/light/assets/authentication/auth-boxed.scss'])
+        @vite(['resources/scss/dark/assets/authentication/auth-boxed.scss'])
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+        <style>
+            #load_screen {
+                display: none;
+            }
+        </style>
+        <!--  END CUSTOM STYLE FILE  -->
+    </x-slot>
+    <!-- END GLOBAL MANDATORY STYLES -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <div class="auth-container d-flex">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        <div class="container mx-auto align-self-center">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+            <div class="row">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
+                    <div class="card mt-3 mb-3">
+                        <div class="card-body">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                            <div class="row">
+                                <div class="col-md-12 mb-3">
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <h5>
+                                        Selamat Datang di <span class="text-uppercase">{{ config('app.name') }}</span>,
+                                    </h5>
+                                    <p>Silahkan masukan email dan kata sandi.</p>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
                                 </div>
-                            </div>
-                        </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                    <div class="col-md-12">
+                                        <div class="mb-3">
+                                            <label class="form-label">Email</label>
+                                            <input id="email" type="text"
+                                                class="form-control @error('email') is-invalid @enderror"
+                                                name="email" value="{{ old('email') }}" required
+                                                autocomplete="email" autofocus>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="mb-4">
+                                            <label class="form-label">Password</label>
+                                            <input id="password" type="password"
+                                                class="form-control @error('password') is-invalid @enderror"
+                                                name="password" required autocomplete="current-password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12">
+                                        <div class="mb-4">
+                                            <button class="btn btn-secondary w-100">MASUK KEDALAM APLIKASI</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
+
                         </div>
-                    </form>
+                    </div>
                 </div>
+
             </div>
+
         </div>
+
     </div>
-</div>
-@endsection
+
+    <!--  BEGIN CUSTOM SCRIPTS FILE  -->
+    <x-slot:footerFiles>
+
+    </x-slot>
+    <!--  END CUSTOM SCRIPTS FILE  -->
+</x-base-layout>
---
+++ resources/views/components/menu/vertical-menu.blade.php
@@ -0,0 +1,66 @@
+{{--
+
+/**
+*
+* Created a new component <x-menu.vertical-menu/>.
+*
+*/
+
+--}}
+
+
+<div class="sidebar-wrapper sidebar-theme">
+
+    <nav id="sidebar">
+
+        <div class="navbar-nav theme-brand flex-row  text-center">
+            <div class="nav-logo">
+                <div class="nav-item theme-text">
+                    <a href="{{ route('home') }}" class="nav-link"> E-Procurement </a>
+                </div>
+            </div>
+            <div class="nav-item sidebar-toggle">
+                <div class="btn-toggle sidebarCollapse">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="feather feather-chevrons-left">
+                        <polyline points="11 17 6 12 11 7"></polyline>
+                        <polyline points="18 17 13 12 18 7"></polyline>
+                    </svg>
+                </div>
+            </div>
+        </div>
+        @if (!Request::is('collapsible-menu/*'))
+            <div class="profile-info">
+                <div class="user-info">
+                    <div class="profile-img">
+                        <img src="{{ Vite::asset('resources/images/profile.png') }}" alt="avatar">
+                    </div>
+                    <div class="profile-content">
+                        <h6 class="">{{ Auth::user()->name }}</h6>
+                        <p class="">{{ Auth::user()->roles->first()->name }}</p>
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        <ul class="list-unstyled menu-categories" id="accordionExample">
+            <li class="menu {{ Request::routeIs('home') ? 'active' : '' }}">
+                <a href="{{ route('home') }}" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round" class="feather feather-home">
+                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                        </svg>
+                        <span>Beranda</span>
+                    </div>
+                </a>
+            </li>
+
+        </ul>
+
+    </nav>
+
+</div>
